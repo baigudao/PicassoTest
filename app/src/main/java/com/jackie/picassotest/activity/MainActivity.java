6e232a6998710a574e1b5ebfb4b700f94da04973
jackie <971060378@qq.com>
2016-12-21 04:07:51 +0800

    四种方式加载图片！补充提交。

--- app/src/main/java/com/jackie/picassotest/activity/MainActivity.java
+++ app/src/main/java/com/jackie/picassotest/activity/MainActivity.java
@@ -1,11 +1,14 @@
 package com.jackie.picassotest.activity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.jackie.picassotest.R;
@@ -18,11 +21,14 @@ import java.io.File;
  */
 public class MainActivity extends Activity {
 
+    public static final String ANDROID_RESOURCE = "android.resource://";
 
     private Button button;
     private Button button_res;
     private Button button_file;
     private ImageView imageView;
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +36,13 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
 
-        button = (Button)findViewById(R.id.btn1);
-        button_res = (Button)findViewById(R.id.btn2);
-        button_file = (Button)findViewById(R.id.btn3);
-        imageView = (ImageView)findViewById(R.id.iv);
+        button = (Button) findViewById(R.id.btn1);
+        button_res = (Button) findViewById(R.id.btn2);
+        button_file = (Button) findViewById(R.id.btn3);
+        imageView = (ImageView) findViewById(R.id.iv);
+        textView = (TextView) findViewById(R.id.tv);
 
-        File file = new File(Environment.getExternalStorageDirectory(),"110720490449.png");
+        File file = new File(Environment.getExternalStorageDirectory(), "110720490449.png");
         Toast.makeText(MainActivity.this, "是否存在图片:" + file.exists(), Toast.LENGTH_SHORT).show();
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -57,16 +64,26 @@ public class MainActivity extends Activity {
             }
         });
 
+        Uri uri = resourceIdToUri(this, R.mipmap.ic_fire);
+        textView.setText(uri.toString());
+        Picasso.with(this).load(uri).into(imageView);
     }
 
-    private void loadImage (){
+
+    private static Uri resourceIdToUri(Context context, int resourceId) {
+        return Uri.parse(ANDROID_RESOURCE + context.getPackageName() + "/" + resourceId);
+    }
+
+
+    private void loadImage() {
         Picasso.with(this).load("http://e.hiphotos.baidu.com/image/pic/item/faf2b2119313b07e86bb728c0fd7912397dd8c2b.jpg").into(imageView);
     }
 
-    private void loadImageRes(){
+    private void loadImageRes() {
         Picasso.with(this).load(R.drawable.bruce).into(imageView);
     }
-    private void loadImageFile(){
-        Picasso.with(this).load(new File(Environment.getExternalStorageDirectory(),"110720490449.png")).into(imageView);
+
+    private void loadImageFile() {
+        Picasso.with(this).load(new File(Environment.getExternalStorageDirectory(), "110720490449.png")).into(imageView);
     }
 }
